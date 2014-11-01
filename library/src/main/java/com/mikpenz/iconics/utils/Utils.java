a4842df93583f897a58e6330c1c01b77909d533b
Mike Penz <mikepenz@gmail.com>
2014-11-01 22:26:33 +0100

    * cleanup Utils class

--- library/src/main/java/com/mikpenz/iconics/utils/Utils.java
+++ library/src/main/java/com/mikpenz/iconics/utils/Utils.java
@@ -16,23 +16,11 @@
 package com.mikpenz.iconics.utils;
 
 import android.content.Context;
-import android.util.Log;
-
-import com.mikpenz.iconics.Iconics;
-
-import java.io.BufferedOutputStream;
-import java.io.Closeable;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 import static android.util.TypedValue.COMPLEX_UNIT_DIP;
 import static android.util.TypedValue.applyDimension;
 
 public class Utils {
-
-    public static final String ICON_FONT_FOLDER = "icon_tmp";
 
     private Utils() {
         // Prevents instantiation
@@ -48,46 +36,5 @@ public class Utils {
             if (state == android.R.attr.state_enabled)
                 return true;
         return false;
-    }
-
-    public static File resourceToFile(Context context, String resourceName) throws IOException {
-        File f = null;
-        if (context.getFilesDir() == null)
-            f = new File(context.getCacheDir(), ICON_FONT_FOLDER);
-        else
-            f = new File(context.getFilesDir(), ICON_FONT_FOLDER);
-        if (!f.exists())
-            if (!f.mkdirs()) {
-                Log.e(Iconics.TAG, "Font folder creation failed");
-                throw new IllegalStateException("Cannot create Iconify font destination folder");
-            }
-        File outPath = new File(f, resourceName);
-        if (outPath.exists()) return outPath;
-
-        BufferedOutputStream bos = null;
-        InputStream inputStream = null;
-        try {
-            inputStream = Iconics.class.getClassLoader().getResourceAsStream(resourceName);
-            byte[] buffer = new byte[inputStream.available()];
-            bos = new BufferedOutputStream(new FileOutputStream(outPath));
-            int l = 0;
-            while ((l = inputStream.read(buffer)) > 0) {
-                bos.write(buffer, 0, l);
-            }
-            return outPath;
-        } finally {
-            closeQuietly(bos);
-            closeQuietly(inputStream);
-        }
-    }
-
-    private static void closeQuietly(Closeable closeable) {
-        if (closeable != null) {
-            try {
-                closeable.close();
-            } catch (IOException e) {
-                // Don't care
-            }
-        }
     }
 }
