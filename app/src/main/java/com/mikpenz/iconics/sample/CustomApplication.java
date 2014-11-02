758e9fb58960a73cdd155759c984805efe956598
Mike Penz <mikepenz@gmail.com>
2014-11-02 11:01:57 +0100

    * add sample typeface-add-on implementation

--- app/src/main/java/com/mikpenz/iconics/sample/CustomApplication.java
+++ app/src/main/java/com/mikpenz/iconics/sample/CustomApplication.java
@@ -5,12 +5,14 @@ import android.app.Application;
 
 import com.mikpenz.iconics.Iconics;
 import com.mikpenz.iconics.sample.typeface.CustomFont;
+import com.mikpenz.meteocons_typeface_library.Meteoconcs;
 
 public class CustomApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Iconics.registerFont(new Meteoconcs());
         Iconics.registerFont(new CustomFont());
     }
 
