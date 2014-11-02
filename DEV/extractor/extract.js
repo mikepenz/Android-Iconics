1a5676af46ef1abcc37ecf86df288f111168ef9a
Mike Penz <mikepenz@gmail.com>
2014-11-02 11:01:02 +0100

    * improve extract.js * move font-awesome extractor to seperate folder

--- DEV/extractor/extract.js
+++ DEV/extractor/extract.js
@@ -7,11 +7,17 @@ fs.readFile("test.css", 'utf8', function(err, data) {
     match = PATTERN_KEY.exec(data);
     match2 = PATTERN_VALUE.exec(data);
     while (match != null) {
-        console.log(match[0] + "('" + match2[0] + "),");
+        var key = match[0];
+        var value = match2[0];
+
+        key = key.replace(/-/g, '_');
+        value = value.replace(/\\e/g, '\\ue');
+
+        console.log(key + "('" + value + "),");
         // matched text: match[0]
         // match start: match.index
         // capturing group n: match[n]
-        match = PATTERN_KEY.exec(data  );
+        match = PATTERN_KEY.exec(data);
         match2 = PATTERN_VALUE.exec(data);
     }
 });
