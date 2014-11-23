61a48b2a976672c4995662c896d911edf2037ca9
Mike Penz <mikepenz@gmail.com>
2014-11-23 21:26:58 +0100

    * extend playground for Google-Material-Design icons

--- app/src/main/java/com/mikpenz/iconics/sample/CustomApplication.java
+++ app/src/main/java/com/mikpenz/iconics/sample/CustomApplication.java
@@ -3,6 +3,7 @@ package com.mikpenz.iconics.sample;
 
 import android.app.Application;
 
+import com.mikpenz.google_material_typeface_library.GoogleMaterial;
 import com.mikpenz.iconics.Iconics;
 import com.mikpenz.iconics.sample.typeface.CustomFont;
 import com.mikpenz.meteocons_typeface_library.Meteoconcs;
@@ -13,6 +14,7 @@ public class CustomApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Iconics.registerFont(new Meteoconcs());
+        Iconics.registerFont(new GoogleMaterial());
         Iconics.registerFont(new CustomFont());
     }
 
