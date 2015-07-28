d23973306cafe9e4e08df5662096ee465347e03f
Mike Penz <mikepenz@gmail.com>
2015-07-28 20:10:23 +0200

    * improve API Level 10 compatiblity of SearchView * hide OpenSource icon from toolbar * reformat code

--- app/src/main/java/com/mikepenz/iconics/sample/MainActivity.java
+++ app/src/main/java/com/mikepenz/iconics/sample/MainActivity.java
@@ -16,7 +16,6 @@
 
 package com.mikepenz.iconics.sample;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -68,55 +67,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
         new DrawerBuilder().withActivity(this)
-                           .withToolbar(toolbar)
-                           .withDrawerItems(items)
-                           .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                               @Override
-                               public boolean onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
-                                   ITypeface font = new ArrayList<>(Iconics.getRegisteredFonts()).get(i);
-                                   loadIcons(font.getFontName());
+                .withToolbar(toolbar)
+                .withDrawerItems(items)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+                        ITypeface font = new ArrayList<>(Iconics.getRegisteredFonts()).get(i);
+                        loadIcons(font.getFontName());
 
-                                   getSupportActionBar().setTitle(font.getFontName());
+                        getSupportActionBar().setTitle(font.getFontName());
 
-                                   return false;
-                               }
-                           })
-                           .withFireOnInitialOnClick(true)
-                           .withSelectedItem(1)
-                           .build();
+                        return false;
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSelectedItem(1)
+                .build();
     }
 
-
-    // TODO: 28.07.2015 IMPORTANT NOTE!
-    // SearchView API >= 11 (will cause errors on API10)
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
 
-        final SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            final SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                search(s);
-                return true;
-            }
-
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                search(s);
-                return true;
-            }
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    search(s);
+                    return true;
+                }
 
 
-            private void search(String s) {
-                if (mIconsFragment != null) mIconsFragment.onSearch(s);
-            }
-        });
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    search(s);
+                    return true;
+                }
+
+
+                private void search(String s) {
+                    if (mIconsFragment != null) mIconsFragment.onSearch(s);
+                }
+            });
+        } else {
+            menu.findItem(R.id.search).setVisible(false);
+        }
 
         MenuItem menuItem = menu.findItem(R.id.action_opensource);
         menuItem.setIcon(new IconicsDrawable(this, FontAwesome.Icon.faw_github).actionBar().color(Color.WHITE));
