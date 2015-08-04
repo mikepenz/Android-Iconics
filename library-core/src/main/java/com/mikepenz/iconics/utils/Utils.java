2655b3539660d513315962db904e9848b3bcde47
Mike Penz <mikepenz@gmail.com>
2015-08-04 23:08:38 +0200

    * simplify the utils class

--- library-core/src/main/java/com/mikepenz/iconics/utils/Utils.java
+++ library-core/src/main/java/com/mikepenz/iconics/utils/Utils.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2013 Joan Zapata
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.mikepenz.iconics.utils;
 
 import android.content.Context;
@@ -21,20 +6,7 @@ import static android.util.TypedValue.COMPLEX_UNIT_DIP;
 import static android.util.TypedValue.applyDimension;
 
 public class Utils {
-
-    private Utils() {
-        // Prevents instantiation
-    }
-
     public static int convertDpToPx(Context context, float dp) {
-        return (int) applyDimension(COMPLEX_UNIT_DIP, dp,
-                context.getResources().getDisplayMetrics());
-    }
-
-    public static boolean isEnabled(int[] stateSet) {
-        for (int state : stateSet)
-            if (state == android.R.attr.state_enabled)
-                return true;
-        return false;
+        return (int) applyDimension(COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
     }
 }
