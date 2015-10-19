cd6f485e4578770ecbaf58e66f1302e485ec68ea
Mike Penz <mikepenz@gmail.com>
2015-10-19 22:54:46 +0200

    * change dependency of core to appcompat-v7 v23.1.0 * implement new IconicsCompatButton THANKS @aacanakin

---
+++ library-core/src/main/java/com/mikepenz/iconics/view/IconicsCompatButton.java
@@ -0,0 +1,40 @@
+package com.mikepenz.iconics.view;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.mikepenz.iconics.Iconics;
+
+public class IconicsCompatButton extends AppCompatButton {
+
+    public IconicsCompatButton(Context context) {
+        super(context);
+    }
+
+    public IconicsCompatButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public IconicsCompatButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void setText(CharSequence text, TextView.BufferType type) {
+        // NOTES:
+        // 1. Need to disable the All Caps option to make Spannable work properly!
+        // 2. This method will be called from the constructor of the super class
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            setAllCaps(false);
+        }
+
+        if (!isInEditMode()) {
+            super.setText(new Iconics.IconicsBuilder().ctx(getContext()).on(text).build(), type);
+        } else {
+            super.setText(text, type);
+        }
+    }
+}
