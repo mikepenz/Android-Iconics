53497c3e4c345be930760f326e64da343168ab00
Mike Penz <mikepenz@gmail.com>
2016-07-26 21:01:41 +0200

    * improve styling * switch to FastAdapter for the icons list

--- app/src/main/java/com/mikepenz/iconics/sample/IconsFragment.java
+++ app/src/main/java/com/mikepenz/iconics/sample/IconsFragment.java
@@ -1,5 +1,8 @@
 package com.mikepenz.iconics.sample;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,15 +11,24 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.IAdapter;
+import com.mikepenz.fastadapter.adapters.FastItemAdapter;
 import com.mikepenz.iconics.Iconics;
-import com.mikepenz.iconics.sample.adapter.IconAdapter;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.iconics.sample.item.IconItem;
 import com.mikepenz.iconics.typeface.ITypeface;
+import com.mikepenz.materialize.util.UIUtils;
 
 import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by a557114 on 16/04/2015.
@@ -24,10 +36,12 @@ import java.util.ArrayList;
 public class IconsFragment extends Fragment {
 
     private static final String FONT_NAME = "FONT_NAME";
-    private ArrayList<String> icons = new ArrayList<String>();
-    private IconAdapter mAdapter;
+    private ArrayList<IconItem> icons = new ArrayList<>();
+    private FastItemAdapter<IconItem> mAdapter;
     private boolean randomize;
     private String search;
+    private PopupWindow popup;
+    private Random random = new Random();
 
     public static IconsFragment newInstance(String fontName) {
         Bundle bundle = new Bundle();
@@ -42,8 +56,7 @@ public class IconsFragment extends Fragment {
     public void randomize(boolean randomize) {
         this.randomize = randomize;
         if (this.mAdapter != null) {
-            this.mAdapter.setRandomized(this.randomize);
-            this.mAdapter.notifyDataSetChanged();
+            this.mAdapter.notifyAdapterDataSetChanged();
         }
     }
 
@@ -61,7 +74,8 @@ public class IconsFragment extends Fragment {
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
         //animator not yet working
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        mAdapter = new IconAdapter(randomize, new ArrayList<String>(), R.layout.row_icon);
+        mAdapter = new FastItemAdapter<>();
+        configAdapter();
         recyclerView.setAdapter(mAdapter);
 
         if (getArguments() != null) {
@@ -71,9 +85,9 @@ public class IconsFragment extends Fragment {
                 if (iTypeface.getFontName().equalsIgnoreCase(fontName)) {
                     if (iTypeface.getIcons() != null) {
                         for (String icon : iTypeface.getIcons()) {
-                            icons.add(icon);
+                            icons.add(new IconItem(icon));
                         }
-                        mAdapter.setIcons(randomize, icons);
+                        mAdapter.set(icons);
                         break;
                     }
                 }
@@ -83,26 +97,118 @@ public class IconsFragment extends Fragment {
         onSearch(search);
     }
 
-    public void onSearch(String s) {
+    private void configAdapter() {
+        //our popup on touch
+        mAdapter.withOnTouchListener(new FastAdapter.OnTouchListener<IconItem>() {
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent, IAdapter<IconItem> adapter, IconItem item, int position) {
+                int a = motionEvent.getAction();
+                if (a == MotionEvent.ACTION_DOWN) {
+                    if (popup != null && popup.isShowing()) {
+                        popup.dismiss();
+                    }
+                    IconicsDrawable icon = new IconicsDrawable(v.getContext()).icon(item.getIcon()).sizeDp(144).paddingDp(8).backgroundColor(Color.parseColor("#DDFFFFFF")).roundedCornersDp(12);
+                    ImageView imageView = new ImageView(v.getContext());
+                    imageView.setImageDrawable(
+                            icon
+                    );
+                    int size = (int) UIUtils.convertDpToPixel(144, v.getContext());
+                    popup = new PopupWindow(imageView, size, size);
+                    popup.showAsDropDown(v);
+
+                    //copy to clipboard
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+                        android.text.ClipboardManager clipboard = (android.text.ClipboardManager) v.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                        clipboard.setText(icon.getIcon().getFormattedName());
+                    } else {
+                        android.content.ClipboardManager clipboard = (android.content.ClipboardManager) v.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                        android.content.ClipData clip = android.content.ClipData.newPlainText("Android-Iconics icon", icon.getIcon().getFormattedName());
+                        clipboard.setPrimaryClip(clip);
+                    }
+                } else if (a == MotionEvent.ACTION_UP || a == MotionEvent.ACTION_CANCEL || a == MotionEvent.ACTION_OUTSIDE) {
+                    if (popup != null && popup.isShowing()) {
+                        popup.dismiss();
+                    }
+                }
+                return false;
+            }
+        });
+
+        mAdapter.withOnBindViewHolderListener(new FastAdapter.OnBindViewHolderListener() {
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+                IconItem.ViewHolder holder = (IconItem.ViewHolder) viewHolder;
+
+                //as we overwrite the default listener
+                mAdapter.getItem(position).bindView(holder);
+
+                if (randomize) {
+                    holder.image.setColorRes(getRandomColor(position));
+                    holder.image.setPaddingDp(random.nextInt(12));
+
+                    holder.image.setContourWidthDp(random.nextInt(2));
+                    holder.image.setContourColor(getRandomColor(position - 2));
+
+
+                    int y = random.nextInt(10);
+                    if (y % 4 == 0) {
+                        holder.image.setBackgroundColorRes(getRandomColor(position - 4));
+                        holder.image.setRoundedCornersDp(2 + random.nextInt(10));
+                    }
+                }
+            }
+        });
+    }
+
+    void onSearch(String s) {
         search = s;
 
         if (mAdapter != null) {
             if (TextUtils.isEmpty(s)) {
                 mAdapter.clear();
-                mAdapter.setIcons(randomize, icons);
-                mAdapter.notifyDataSetChanged();
+                mAdapter.setNewList(icons);
             } else {
-                AbstractList<String> tmpList = new ArrayList<>();
-                for (String icon : icons) {
-                    if (icon.toLowerCase().contains(s.toLowerCase())) {
+                AbstractList<IconItem> tmpList = new ArrayList<>();
+                for (IconItem icon : icons) {
+                    if (icon.getIcon().toLowerCase().contains(s.toLowerCase())) {
                         tmpList.add(icon);
                     }
                 }
-                mAdapter.clear();
-                mAdapter.setIcons(randomize, tmpList);
-                mAdapter.notifyDataSetChanged();
+                mAdapter.setNewList(tmpList);
             }
         }
     }
 
+
+    private int getRandomColor(int i) {
+        //make sure w are > 0
+        if (i < 0) {
+            i = i * (-1);
+        }
+
+        //get a random color
+        if (i % 10 == 0) {
+            return R.color.md_black_1000;
+        } else if (i % 10 == 1) {
+            return R.color.md_blue_500;
+        } else if (i % 10 == 2) {
+            return R.color.md_green_500;
+        } else if (i % 10 == 3) {
+            return R.color.md_red_500;
+        } else if (i % 10 == 4) {
+            return R.color.md_orange_500;
+        } else if (i % 10 == 5) {
+            return R.color.md_pink_500;
+        } else if (i % 10 == 6) {
+            return R.color.md_amber_500;
+        } else if (i % 10 == 7) {
+            return R.color.md_blue_grey_500;
+        } else if (i % 10 == 8) {
+            return R.color.md_orange_500;
+        } else if (i % 10 == 9) {
+            return R.color.md_yellow_500;
+        }
+
+        return 0;
+    }
 }
---
+++ app/src/main/java/com/mikepenz/iconics/sample/item/IconItem.java
@@ -0,0 +1,97 @@
+package com.mikepenz.iconics.sample.item;
+
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.mikepenz.fastadapter.items.AbstractItem;
+import com.mikepenz.fastadapter.utils.ViewHolderFactory;
+import com.mikepenz.iconics.sample.R;
+import com.mikepenz.iconics.view.IconicsImageView;
+
+/**
+ * Created by mikepenz on 26.07.16.
+ */
+
+public class IconItem extends AbstractItem<IconItem, IconItem.ViewHolder> {
+    //the static ViewHolderFactory which will be used to generate the ViewHolder for this Item
+    private static final ViewHolderFactory<? extends ViewHolder> FACTORY = new ItemFactory();
+
+    private String icon;
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public IconItem withIcon(String icon) {
+        this.icon = icon;
+        return this;
+    }
+
+    public IconItem(String icon) {
+        this.icon = icon;
+    }
+
+    @Override
+    public int getType() {
+        return R.id.item_row_icon;
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.row_icon;
+    }
+
+
+    @Override
+    public void bindView(ViewHolder holder) {
+        super.bindView(holder);
+
+        holder.image.setIcon(icon);
+        holder.name.setText(icon);
+
+        holder.image.setColor(Color.BLACK);
+        holder.image.setPaddingDp(0);
+        holder.image.setContourWidthDp(0);
+        holder.image.setContourColor(Color.TRANSPARENT);
+        holder.image.setBackgroundColor(Color.TRANSPARENT);
+
+        //as we want to respect the bounds of the original font in the icon list
+        holder.image.getIcon().respectFontBounds(true);
+    }
+
+    /**
+     * our ItemFactory implementation which creates the ViewHolder for our adapter.
+     * It is highly recommended to implement a ViewHolderFactory as it is 0-1ms faster for ViewHolder creation,
+     * and it is also many many times more efficient if you define custom listeners on views within your item.
+     */
+    protected static class ItemFactory implements ViewHolderFactory<ViewHolder> {
+        public ViewHolder create(View v) {
+            return new ViewHolder(v);
+        }
+    }
+
+    /**
+     * return our ViewHolderFactory implementation here
+     *
+     * @return
+     */
+    @Override
+    public ViewHolderFactory<? extends ViewHolder> getFactory() {
+        return FACTORY;
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public IconicsImageView image;
+
+
+        ViewHolder(final View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.name);
+            image = (IconicsImageView) itemView.findViewById(R.id.icon);
+        }
+    }
+}
