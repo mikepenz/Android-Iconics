a2f28c20e10fc86f2e267c1cefffe401f04d7f88
Mike Penz <mikepenz@gmail.com>
2020-11-23 20:10:09 +0100

    - disable release build for now

--- release.sh
+++ release.sh
@@ -4,24 +4,24 @@
 
 if [ "$1" = "release" ];
 then
-    ./gradlew bintrayUpload -Plibrarytypefaceonly
+    ./gradlew bintrayUpload -x test -x lint -Plibrarytypefaceonly
     ./gradlew bintrayUpload -x test -x lint -Plibrarycoreonly
     ./gradlew bintrayUpload -x test -x lint -Plibraryviewsonly
 
-    #./gradlew build bintrayUpload -x test -x lint -Pcommunitymaterialonly
-    #./gradlew build bintrayUpload -x test -x lint -PDevIcononly
-    #./gradlew build bintrayUpload -x test -x lint -PEntypoonly
-    #./gradlew build bintrayUpload -x test -x lint -Pfontawesomeonly
-    #./gradlew build bintrayUpload -x test -x lint -PFoundationIconsonly
-    #./gradlew build bintrayUpload -x test -x lint -Pgooglematerialonly
-    #./gradlew build bintrayUpload -x test -x lint -PIoniconsonly
-    #./gradlew build bintrayUpload -x test -x lint -Pmaterialdesigniconiconly
-    #./gradlew build bintrayUpload -x test -x lint -Pmeteoconsonly
-    #./gradlew build bintrayUpload -x test -x lint -Pocticons
-    #./gradlew build bintrayUpload -x test -x lint -PPixeden7Strokeonly
-    #./gradlew build bintrayUpload -x test -x lint -PTypeiconsonly
-    #./gradlew build bintrayUpload -x test -x lint -Pweathericonsonly
-    #./gradlew build bintrayUpload -x test -x lint -Pmaterialdesigndxonly
+    # ./gradlew bintrayUpload -x test -x lint -Pcommunitymaterialonly
+    # ./gradlew bintrayUpload -x test -x lint -PDevIcononly
+    # ./gradlew bintrayUpload -x test -x lint -PEntypoonly
+    # ./gradlew bintrayUpload -x test -x lint -Pfontawesomeonly
+    # ./gradlew bintrayUpload -x test -x lint -PFoundationIconsonly
+    # ./gradlew bintrayUpload -x test -x lint -Pgooglematerialonly
+    # ./gradlew bintrayUpload -x test -x lint -PIoniconsonly
+    # ./gradlew bintrayUpload -x test -x lint -Pmaterialdesigniconiconly
+    # ./gradlew bintrayUpload -x test -x lint -Pmeteoconsonly
+    # ./gradlew bintrayUpload -x test -x lint -Pocticons
+    # ./gradlew bintrayUpload -x test -x lint -PPixeden7Strokeonly
+    # ./gradlew bintrayUpload -x test -x lint -PTypeiconsonly
+    # ./gradlew bintrayUpload -x test -x lint -Pweathericonsonly
+    # ./gradlew bintrayUpload -x test -x lint -Pmaterialdesigndxonly
 else
     echo todo
 fi
